e20914e45e47595878f8acd42669b7868829a679
karanmandal121 <105425364+karanmandal121@users.noreply.github.com>
2023-07-17 22:04:00 +0530

    script.js
    Weather js

---
+++ script.js
@@ -0,0 +1,34 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ce791b3089msh0b1e1fbbb715401p1242c2jsn7400b0732112',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+ city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            //cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            temp2.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.min_temp
+            humidity2.innerHTML = response.min_temp
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_speed2.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+        })
+        .catch(err => console.error(err));
+}
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+getWeather("")
